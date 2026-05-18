8a0c0d771078698a3e9d89346e18770f9ab68c4e
AManaswini <manaswiniaitha55@gmail.com>
2026-05-17 22:16:40 -0700

    closed source chat models

---
+++ 2.ChatModels/1_chatmodel_openai.py
@@ -0,0 +1,9 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatOpenAI(model = 'gpt-4', temperature = 1, max_completion_tokens = 10)
+result = model.invoke('Give me a pickup line to hit on a boy')
+
+print(result.content)
